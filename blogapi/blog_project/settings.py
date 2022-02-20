d4de3232e5aab30e72b00a7a24ee25cd773aaede
crystal <crstalokd@gmail.com>
2022-02-20 13:25:11 +0100

    authentication & docs

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'posts',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_swagger', 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth', 
+    'rest_auth.registration', 
 ]
 
 MIDDLEWARE = [
@@ -125,5 +133,19 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES': [
 'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+
+'DEFAULT_AUTHENTICATION_CLASSES': [ # new
+'rest_framework.authentication.SessionAuthentication',
+'rest_framework.authentication.TokenAuthentication', 
+],
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
+
+SWAGGER_SETTINGS = {
+'LOGIN_URL': 'rest_framework:login',
+'LOGOUT_URL': 'rest_framework:logout',
 }
